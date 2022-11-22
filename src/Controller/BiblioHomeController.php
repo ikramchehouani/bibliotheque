3364f250db32f82e13fa9b83120b672daea64e5a
Ikram CHEHOUANI <Ikram.CHEHOUANI@ICAPE.local>
2022-11-22 14:21:30 +0100

    v1 home done

---
+++ src/Controller/BiblioHomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BiblioHomeController extends AbstractController
+{
+    #[Route('/biblio/home', name: 'app_biblio_home')]
+    public function index(): Response
+    {
+        return $this->render('biblio_home/index.html.twig', [
+            'controller_name' => 'BiblioHomeController',
+        ]);
+    }
+}
